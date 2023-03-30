744d2be02d5d9703c34cd809bbad1fc5fc4ec3d1
nergizz <memmedzadenergizw@gmail.com>
2023-03-30 18:49:28 +0400

    mesaj

---
+++ feistel folder browser/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace feistel_folder_browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(578, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(354, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 31);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(607, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Restart";
+            this.label3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label3_MouseClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(607, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Pausa";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 399);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
+
---
+++ feistel folder browser/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace feistel_folder_browser
+{
+    public partial class Form1 : Form
+    {
+        int xx=20,yy=0,yemx=0,yemy=0,randx=0,randy=0,addim=20;
+        int c=1;
+        int[] x = new int[1];
+        int[] y = new int[1];
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //label5.Text = "";
+            Graphics l = e.Graphics;
+            Pen p = new Pen(Color.Black, 5);
+            for (int i = 0; i <c; i++)
+            {
+                
+                e.Graphics.FillRectangle((Brushes.Blue), x[i], y[i], addim, addim);
+			}
+            e.Graphics.FillRectangle((Brushes.Red), yemx, yemy, addim, addim);
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //x[0] = 100; y[0] = 100;
+            x[0] = (rand.Next(1, 18)) * addim;
+            y[0] = (rand.Next(1, 18)) * addim;
+            randx = rand.Next(1, 20);
+            randy = rand.Next(1, 20);
+            yemx = randx * addim;
+            yemy = randy * addim;
+           
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData==Keys.Right)
+            {
+                
+                xx = addim;
+                yy = 0;
+            }
+            if (e.KeyData == Keys.Left)
+            {
+              
+                xx = -addim;
+                yy = 0;
+
+            }
+            if (e.KeyData == Keys.Up)
+            {
+                xx = 0;
+                yy = -addim;
+             
+            } 
+            if (e.KeyData == Keys.Down)
+            {
+                xx = 0;
+                yy = addim;
+                
+            }
+            if (e.KeyData==Keys.Shift)
+            {
+                timer1.Interval = 100;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+               
+           
+            for (int i = x.Length - 1; i > 0; i--)
+            {
+                x[i] = x[i - 1];
+                y[i] = y[i - 1];
+            }
+            
+            
+            x[0] = x[0] + xx;
+            y[0] = y[0] + yy;
+            for (int i = 0; i < c; i++)
+            {
+                if ((x[i] > this.Width) || (x[i] < 0) || (y[i] > this.Height) || (y[i] < 0))
+                {
+                    timer1.Stop();
+                    label2.Text = "oyun bitti";
+
+                }
+                //if ((c > 2)&&(i>=1))
+                //{
+                //    if ((x[i] == x[0]) && (y[i] == y[0]) && (!(x[1] == x[0]) || ((y[1] == y[0]))))
+                //    {
+                //        timer1.Stop();
+                //        label2.Text = "oyun bitti";
+                //    }
+                //}
+                
+            }
+            for (int i = 1; i < c; i++)
+            {
+
+                if (c > 2)
+                {
+                    if ((x[i] == x[0]) && (y[i] == y[0]) && (!(x[1] == x[0]) && ((y[1] == y[0]))))
+                    {
+                        timer1.Stop();
+                        label2.Text = "oyun bitti";
+                    }
+                }
+            }
+            
+            
+            
+            for (int i = 0; i < x.Length; i++)
+            {
+                        if (i==0)
+	                {
+		              
+                    } 
+                if (i>0)
+                        {
+                                if ((x[i]==x[0])&&(y[i]==y[0]))
+                            {
+                                 
+                            }
+                        }
+                        
+               
+
+            }
+            if ((x[0] == yemx) && (y[0] == yemy))
+            {
+
+                c = c + 1;
+
+                label1.Text = c.ToString();
+                Array.Resize(ref x, c + 1);
+                Array.Resize(ref y, c + 1);
+
+                x[0] = yemx;
+                y[0] = yemy;
+                randx = rand.Next(1, 20);
+                randy = rand.Next(1, 20);
+                yemx = randx * addim;
+                yemy = randy * addim;
+            }
+            
+
+            
+            Invalidate();
+        }
+
+        
+
+        private void label3_MouseClick(object sender, MouseEventArgs e)
+        {
+            int xx = 20, yy = 0, yemx = 0, yemy = 0, randx = 0, randy = 0, addim = 20;
+             c = 1;
+            Array.Resize(ref x, 1);
+            Array.Resize(ref y, 1);
+            Random rand = new Random();
+           Form1_Load(sender, e);
+           timer1.Start();
+           label1.Text = "";
+           label2.Text = "";
+
+            
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+            if ( label4.Text == "pausa")
+            { timer1.Stop();
+            label4.Text = "continue";
+            }
+            else
+            {
+                label4.Text = "pausa";
+                timer1.Start();
+            }
+           
+        }
+
+        
+
+       
+
+    }
+}
+
